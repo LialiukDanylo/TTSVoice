34ec476dca184bc7183618d75c363264df2d3496
LialiukDanylo <lialiukdanylo@gmail.com>
2026-03-06 00:01:17 +0100

    feat: add input field and test speak command

---
+++ src/TTSVoice.UI/ViewModels/MainWindowVM.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
+
+namespace TTSVoice.UI.ViewModels
+{
+    partial class MainWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SpeakCommand))]
+        private string _inputText = string.Empty;
+
+        [RelayCommand(CanExecute = nameof(CanSpeak))]
+        private async Task Speak()
+        {
+            Debug.WriteLine(InputText);
+        }
+        private bool CanSpeak => !string.IsNullOrWhiteSpace(InputText);
+    }
+}
